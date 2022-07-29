9a7a8991fba53e87d9f70baee032a7aacb679967
Enrique <santosenrique2121@gmail.com>
2022-07-29 10:39:35 -0300

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <strong>Template eslint</strong>
+      </div>
+    </div>
+  );
+}
+
+export default App;
